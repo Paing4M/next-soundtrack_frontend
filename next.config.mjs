98a978a2985b617ae8f1a67120751145fd3de812
Paing4M <paing.2000@yahoo.com>
2024-07-30 09:44:07 +0630

    config

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,9 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
-				protocol: 'http',
-				hostname: '127.0.0.1',
-				port: '8000',
+				protocol: 'https',
+				hostname: 'music-stream.p4m.serv00.net',
+				
 			},
 		],
 	},
